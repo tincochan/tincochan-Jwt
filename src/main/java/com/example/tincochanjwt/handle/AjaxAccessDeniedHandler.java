c0c5b8955105ff840a666610e03305ca1c912aac
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2021-07-13 17:12:21 +0800

    spring security权限不足处理类

---
+++ src/main/java/com/example/tincochanjwt/handle/AjaxAccessDeniedHandler.java
@@ -0,0 +1,20 @@
+package com.example.tincochanjwt.handle;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AjaxAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write(JSON.toJSONString("权限不足"));
+    }
+}
