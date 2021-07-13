11b9c0105886adb2dd661ddb886902cad1c1dc21
Tinc0ist <34474781+tincochan@users.noreply.github.com>
2021-07-13 17:41:22 +0800

    匿名无权限处理类

---
+++ src/main/java/com/example/tincochanjwt/handle/AjaxAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.example.tincochanjwt.handle;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description: 匿名未认证登录处理类
+ */
+@Component
+public class AjaxAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setContentType("application/json;charset=utf-8");
+        request.getRequestDispatcher("/login.html").forward(request,response);
+    }
+}
